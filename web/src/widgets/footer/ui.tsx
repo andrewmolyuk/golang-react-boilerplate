82722b9dfd0ed98e9c6920710624609725389e26
Andrew Molyuk <andrew@molyuk.com>
2024-03-31 00:18:17 +0300

    Add dayjs and format build date in the footer

--- web/src/widgets/footer/ui.tsx
+++ web/src/widgets/footer/ui.tsx
@@ -1,12 +1,14 @@
 import { Center, Text } from '@mantine/core';
+import dayjs from 'dayjs';
 import pkg from '~/../package.json';
 
 export const Footer = () => {
+  const title = `Build ${pkg.version}-${pkg.buildRef} · ${dayjs(pkg.buildDate).format('DD-MM-YYYY HH:mm:ss')}`;
   return (
     <footer>
       <Center w={'100%'} bg={'dark'} h={'50'}>
-        <Text c={'white'}>
-          2024 &copy; Andrew Molyuk &middot; Build {pkg.version}-{pkg.buildRef} &middot; {pkg.buildDate}
+        <Text c={'white'} title={title}>
+          2024 &copy; Andrew Molyuk
         </Text>
       </Center>
     </footer>
